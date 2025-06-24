84388b11eca4fb74fe7f4cdfc33ef9cc527bd600
Bálint Ducsai <balint.ducsai@gmail.com>
2025-06-24 11:30:53 +0200

    test(env): add tests for isProdEnv and isStageEnv functions

--- src/tests/unit/helpers.spec.ts
+++ src/tests/unit/helpers.spec.ts
@@ -1,8 +1,16 @@
+// @vitest-environment node
+
 import { describe, expect, test, vi } from "vitest";
 
 import { LOCAL_PICKUP_ADDRESS } from "~/lib/constants";
-import { delay, normalizeOrderForm } from "~/lib/helpers";
+import {
+  delay,
+  isProdEnv,
+  isStageEnv,
+  normalizeOrderForm,
+} from "~/lib/helpers";
 import type { OrderForm } from "~/lib/validation-schemas";
+import * as envModule from "~/lib/env";
 
 test("delay - resolves after the given time", async () => {
   const delayTime = 6000;
@@ -78,5 +86,54 @@ describe("normalizeOrderForm", () => {
     const normalizedOrderFormData = normalizeOrderForm(formData);
 
     expect(normalizedOrderFormData).toBe(formData);
+  });
+});
+
+describe("isProdEnv", () => {
+  test("should return false if NODE_ENV or VERCEL_ENV not production ", () => {
+    vi.stubEnv("NODE_ENV", "test");
+
+    expect(isProdEnv()).toBe(false);
+
+    vi.stubEnv("NODE_ENV", "production");
+
+    expect(isProdEnv()).toBe(false);
+
+    vi.unstubAllEnvs();
+  });
+
+  test("should return true if NODE_ENV and VERCEL_ENV is production", () => {
+    vi.spyOn(envModule, "env", "get").mockReturnValue({
+      ...envModule.env,
+      VERCEL_ENV: "production",
+    });
+
+    vi.stubEnv("NODE_ENV", "production");
+
+    expect(isProdEnv()).toBe(true);
+
+    vi.unstubAllEnvs();
+  });
+});
+
+describe("isStageEnv", () => {
+  test("should return false if NODE_ENV is not production", () => {
+    vi.stubEnv("NODE_ENV", "test");
+
+    expect(isStageEnv()).toBe(false);
+  });
+
+  test("should return true if VERCEL_ENV is stage and VERCEL_GIT_PULL_REQUEST_ID is set", () => {
+    vi.spyOn(envModule, "env", "get").mockReturnValue({
+      ...envModule.env,
+      VERCEL_ENV: "stage",
+      VERCEL_GIT_PULL_REQUEST_ID: "12345",
+    });
+
+    vi.stubEnv("NODE_ENV", "production");
+
+    expect(isStageEnv()).toBe(true);
+
+    vi.unstubAllEnvs();
   });
 });
