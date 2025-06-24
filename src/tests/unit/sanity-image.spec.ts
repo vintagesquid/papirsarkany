14877b18c3233b7fb71910f8e291e5a2d234b2b8
Bálint Ducsai <balint.ducsai@gmail.com>
2025-06-24 11:30:41 +0200

    test(unit): add sanity-image spec

---
+++ src/tests/unit/sanity-image.spec.ts
@@ -0,0 +1,19 @@
+import { expect, test } from "vitest";
+
+import { getPositionFromHotspot, urlFor } from "~/lib/sanity-image";
+import { kiteMock } from "~/mocks/product.mock";
+
+test("urlfor", () => {
+  expect(urlFor(kiteMock.image)).toBeDefined();
+
+  expect(urlFor(kiteMock.image)).toHaveProperty("url");
+});
+
+test("getPositionFromHotspot", () => {
+  expect(getPositionFromHotspot()).toBeDefined();
+
+  expect(getPositionFromHotspot()).toBe("center");
+  expect(getPositionFromHotspot(kiteMock.image.hotspot)).toBe(
+    `${kiteMock.image.hotspot.x * 100}% ${kiteMock.image.hotspot.y * 100}%`,
+  );
+});
