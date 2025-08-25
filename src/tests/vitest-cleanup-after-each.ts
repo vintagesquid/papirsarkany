f9f9eb2d37ade9884dfb6f5404538063801d6e1e
Bálint Ducsai <balint.ducsai@gmail.com>
2025-08-25 15:35:55 +0200

    test(unit): clear all timers after each test

--- src/tests/vitest-cleanup-after-each.ts
+++ src/tests/vitest-cleanup-after-each.ts
@@ -1,6 +1,7 @@
 import { cleanup } from "@testing-library/react";
-import { afterEach } from "vitest";
+import { afterEach, vitest } from "vitest";
 
 afterEach(() => {
   cleanup();
+  vitest.clearAllTimers();
 });
