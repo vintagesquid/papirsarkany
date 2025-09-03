14c130e653cd74af415779c4a817480a9970286c
Bálint Ducsai <balint.ducsai@gmail.com>
2025-09-03 14:58:02 +0200

    chore: move typed routes to stable config

--- next.config.js
+++ next.config.js
@@ -6,9 +6,7 @@ const withBundleAnalyzer = NextBundleAnalyzer({
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    typedRoutes: true,
-  },
+  typedRoutes: true,
   webpack(config) {
     const fileLoaderRule = config.module.rules.find((rule) =>
       rule.test?.test?.(".svg"),
