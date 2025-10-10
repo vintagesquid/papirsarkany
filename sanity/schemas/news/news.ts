2d2337b34442c5a63249c333fb76c48b1dadb086
Bálint Ducsai <balint.ducsai@gmail.com>
2025-10-10 20:03:28 +0200

    refoctor(news): use defineField function

--- sanity/schemas/news/news.ts
+++ sanity/schemas/news/news.ts
@@ -1,21 +1,24 @@
-import { defineType } from "sanity";
+import { defineField, defineType } from "sanity";
 
 export default defineType({
   name: "news",
   type: "document",
   title: "Hírek",
   fields: [
-    {
+    defineField({
       name: "label",
       type: "string",
       title: "Cím",
       validation: (rule) => rule.required(),
-    },
-    {
+    }),
+    defineField({
       name: "link",
       type: "url",
       title: "Link",
-      validation: (rule) => rule.optional().uri(),
-    },
+      validation: (Rule) =>
+        Rule.uri({
+          scheme: ["http", "https", "mailto", "tel"],
+        }),
+    }),
   ],
 });
