108850223a846cd6437e8b874de531759e1bfc43
Bálint Ducsai <balint.ducsai@gmail.com>
2025-12-06 19:16:25 +0100

    feat: migrate to @sanity/image-url v2.0.0

--- src/lib/sanity-image.ts
+++ src/lib/sanity-image.ts
@@ -1,10 +1,12 @@
-import imageUrlBuilder from "@sanity/image-url";
-import type { SanityImageSource } from "@sanity/image-url/lib/types/types";
+import {
+  createImageUrlBuilder,
+  type SanityImageHotspot,
+  type SanityImageSource,
+} from "@sanity/image-url";
 import { client } from "@sanity/lib/client";
-import type { SanityImageHotspot } from "@sanity/lib/sanity.types";
 import type { ImageUrlBuilder } from "sanity";
 
-const builder = imageUrlBuilder(client);
+const builder = createImageUrlBuilder(client);
 
 export function urlFor(source: SanityImageSource): ImageUrlBuilder {
   return builder.image(source);
