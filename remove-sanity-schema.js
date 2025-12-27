df3a9443281e063bacfd9450b79dc6073d7daa8b
Bálint Ducsai <balint.ducsai@gmail.com>
2025-12-27 20:47:49 +0100

    feat(typegen): better handling for missing sanity schema file

--- remove-sanity-schema.js
+++ remove-sanity-schema.js
@@ -3,5 +3,9 @@ import fs from "node:fs";
 try {
   fs.rmSync("./sanity/schema.json");
 } catch (error) {
-  console.log(error);
+  if (error.code === 'ENOENT') {
+    console.log('Sanity schema file does not exist.')
+  } else {
+    console.log(error);
+  }
 }
