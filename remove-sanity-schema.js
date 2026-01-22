e9f395401d80193a071502e241128cdfe1b652b6
Bálint Ducsai <balint.ducsai@gmail.com>
2026-01-22 22:31:53 +0100

    style: run format

--- remove-sanity-schema.js
+++ remove-sanity-schema.js
@@ -3,8 +3,8 @@ import fs from "node:fs";
 try {
   fs.rmSync("./sanity/schema.json");
 } catch (error) {
-  if (error.code === 'ENOENT') {
-    console.log('Sanity schema file does not exist.')
+  if (error.code === "ENOENT") {
+    console.log("Sanity schema file does not exist.");
   } else {
     console.log(error);
   }
