3efd76eb615503ebfb3f4e510057ee041957ff20
Bálint Ducsai <balint.ducsai@gmail.com>
2026-02-02 22:33:44 +0100

    feat(home): remove Google map iframe

--- src/app/(shop)/page.tsx
+++ src/app/(shop)/page.tsx
@@ -117,14 +117,6 @@ export default async function HomePage() {
               <Heading as={"h4"}>{contact.email}</Heading>
               <Heading as={"h4"}>{contact.phoneNumber}</Heading>
             </div>
-
-            <iframe
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1131.6105816797208!2d18.87852618960264!3d47.57870829362802!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xceefc53f4870d42e!2swww.papirsarkany.hu!5e0!3m2!1shu!2shu!4v1579197549648!5m2!1shu!2shu"
-              className="mx-auto h-[600px] w-full max-w-6xl rounded-xl border-3"
-              loading="lazy"
-              referrerPolicy="no-referrer-when-downgrade"
-              title="Google map"
-            />
           </HomeSection.Content>
         </HomeSection>
       </div>
