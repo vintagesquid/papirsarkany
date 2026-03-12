b43aaee6f8ef18537770a558151e9d0bf2cbbbea
Bálint Ducsai <balint.ducsai@gmail.com>
2026-03-13 00:49:11 +0100

    fix(db): set max weight to 5s

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -9,6 +9,9 @@ const prisma =
   globalThis.prisma ||
   new PrismaClient({
     adapter: new PrismaNeon({ connectionString: process.env.DATABASE_URL }),
+    transactionOptions: {
+      maxWait: 5000,
+    },
   });
 
 if (process.env.NODE_ENV === "development") {
